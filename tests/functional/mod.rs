6eb20512c88e33b5e78507fe84313ff7610ea354
Jeremy Justus <jeremyjustus0916@gmail.com>
2026-05-22 19:59:01 -0400

    Add H1 body limit zero coverage

--- tests/functional/mod.rs
+++ tests/functional/mod.rs
@@ -39,6 +39,7 @@ mod functional_plugin_extended_test;
 mod functional_plugin_test;
 mod functional_plugins_network_test;
 mod functional_prometheus_test;
+mod functional_protocol_body_limit_config_test;
 mod functional_protocol_validation_test;
 mod functional_redis_rate_limiting_test;
 mod functional_regex_routing_test;
