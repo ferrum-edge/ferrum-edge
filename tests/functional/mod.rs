94c525b341ce6f6a5b176bd77438993b15d3ec67
Jeremy Justus <jeremyjustus0916@gmail.com>
2026-05-22 16:22:18 -0400

    Add h3 authority validation coverage

--- tests/functional/mod.rs
+++ tests/functional/mod.rs
@@ -19,6 +19,7 @@ mod functional_file_mode_test;
 mod functional_graceful_shutdown_test;
 mod functional_grpc_plugins_test;
 mod functional_grpc_test;
+mod functional_h3_authority_validation_test;
 mod functional_host_only_routing_test;
 mod functional_load_balancer_test;
 mod functional_load_stress_test;
