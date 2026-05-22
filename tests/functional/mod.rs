e4e7cf52dd489a791d4e1b6d01bef1def791295d
Jeremy Justus <jeremyjustus0916@gmail.com>
2026-05-22 16:02:04 -0400

    Add websocket frame limit coverage

--- tests/functional/mod.rs
+++ tests/functional/mod.rs
@@ -54,6 +54,7 @@ mod functional_tcp_proxy_test;
 mod functional_tls_lifecycle_test;
 mod functional_tls_only_test;
 mod functional_udp_proxy_test;
+mod functional_websocket_limits_test;
 mod functional_websocket_test;
 mod functional_ws_plugins_test;
 mod namespace_helpers;
