24958872015ea7423f6444989a87bbbe2541784e
Jeremy Justus <jeremyjustus0916@gmail.com>
2026-05-23 00:42:06 -0400

    Add early data protocol tests

--- tests/functional/mod.rs
+++ tests/functional/mod.rs
@@ -20,6 +20,7 @@ mod functional_db_outage_test;
 mod functional_db_tls_test;
 mod functional_db_upstream_test;
 mod functional_dns_cache_test;
+mod functional_early_data_test;
 mod functional_file_mode_test;
 mod functional_forwarded_via_headers_test;
 mod functional_graceful_shutdown_test;
