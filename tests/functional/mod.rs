4e7389743a98add5ea0cd4ecb2775a619d013c3f
Jeremy Justus <jeremyjustus0916@gmail.com>
2026-05-22 23:38:58 -0400

    Add streaming tracking size limit coverage

--- tests/functional/mod.rs
+++ tests/functional/mod.rs
@@ -59,6 +59,7 @@ mod functional_service_discovery_test;
 mod functional_shared_harness_smoke_test;
 mod functional_sse_test;
 mod functional_stream_listener_failure_test;
+mod functional_streaming_latency_tracking_test;
 mod functional_tcp_proxy_test;
 mod functional_tls_lifecycle_test;
 mod functional_tls_only_test;
