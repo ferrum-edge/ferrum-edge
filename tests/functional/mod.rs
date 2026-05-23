6ed7185cb6a0a9ce8a76b3dbdecb25b6efe339a8
Jeremy Justus <jeremyjustus0916@gmail.com>
2026-05-22 21:02:14 -0400

    Fix H3 request body bridge EOF

--- tests/functional/mod.rs
+++ tests/functional/mod.rs
@@ -42,6 +42,7 @@ mod functional_prometheus_test;
 mod functional_protocol_body_limit_config_test;
 mod functional_protocol_h2_body_limit_config_test;
 mod functional_protocol_h3_body_limit_config_test;
+mod functional_protocol_h3_request_body_test;
 mod functional_protocol_validation_test;
 mod functional_redis_rate_limiting_test;
 mod functional_regex_routing_test;
