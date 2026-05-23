21de384d6fe9d90dc11b19e1395e13f017445b75
Jeremy Justus <jeremyjustus0916@gmail.com>
2026-05-22 23:12:05 -0400

    Add URL query env protocol coverage

--- tests/functional/mod.rs
+++ tests/functional/mod.rs
@@ -61,6 +61,7 @@ mod functional_tls_lifecycle_test;
 mod functional_tls_only_test;
 mod functional_udp_proxy_test;
 mod functional_websocket_limits_test;
+mod functional_url_query_limits_test;
 mod functional_websocket_test;
 mod functional_ws_plugins_test;
 mod namespace_helpers;
